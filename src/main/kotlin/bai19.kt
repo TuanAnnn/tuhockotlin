ae7bf23f7463fc829e1d3476db23d96ae63941c3
TuanAn <tuananthptcc10a5@gmail.com>
2023-07-07 18:12:18 +0700

    bai 18,19

---
+++ src/main/kotlin/bai19.kt
@@ -0,0 +1,37 @@
+fun main(args: Array<String>) {
+//    var kytu:Char = 'a';
+//    println(kytu)
+//    println(kytu::class.java.typeName)
+//
+//    // in ky tu bang ma Ascii
+//    var kytu2 = Char(65);
+//    println(kytu2)
+//    var kytu3 = Char(110);
+//    println(kytu3)
+
+    //ep kieu cho kieu char
+//    println("Enter a word: ")
+//    var s:String? = readLine()
+//    if(s==null) return
+//    var kt:Char = s.first()
+//    var kt2:Char = s[0]
+//    var  kt3:Char = s.single()
+
+//    println(kt)
+//    println(kt2)
+//    println(kt3)
+//    println(kt::class.java.typeName)
+//    println(kt2::class.java.typeName)
+//    println(kt3::class.java.typeName)
+
+//    // so sanh
+//    println('C'.compareTo('D'))
+
+//    //so sanh
+//    println('a'.equals('A'))
+
+    //method
+    println('8'.isDigit())
+    println('A'.isLowerCase())
+    println(' '.isWhitespace())
+}
